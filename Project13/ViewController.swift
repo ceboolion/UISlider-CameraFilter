56797a059f0057ad2acd5f2938714c9a3c389263
rcebul01 <roman.cebula@isobar.com>
2020-05-12 10:34:05 +0200

    Initial commit
    Creation of UI

---
+++ Project13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Roman Cebula on 12/05/2020.
+//  Copyright © 2020 Roman Cebula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var intensity: UISlider!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func changeFilter(_ sender: Any) {
+  }
+  
+  @IBAction func save(_ sender: Any) {
+  }
+  
+  @IBAction func intensityChanged(_ sender: Any) {
+  }
+  
+  
+}
+
